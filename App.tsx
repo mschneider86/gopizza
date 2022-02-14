c2efed665b8c4fbd97c39d365d9b2fb24509e960
Matheus Schneider <matheus.ssm@gmail.com>
2022-02-14 16:20:18 -0300

    add: app theme

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import { View } from 'react-native';
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -14,5 +17,9 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <View></View>;
+  return (
+    <ThemeProvider theme={theme}>
+      <View></View>
+    </ThemeProvider>
+  );
 }
