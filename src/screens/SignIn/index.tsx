0d342b03caf3cc1079df1ed04eff5b224497d43b
Matheus Schneider <matheus.ssm@gmail.com>
2022-02-15 17:57:17 -0300

    add: Button component

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Input } from '@components/Input';
 import { Container } from './styles';
+import { Button } from '@components/Button';
 
 export function SignIn() {
   return (
@@ -14,6 +15,8 @@ export function SignIn() {
       />
 
       <Input placeholder='Senha' type='secondary' secureTextEntry />
+
+      <Button title='Entrar' type='secondary' />
     </Container>
   );
 }
