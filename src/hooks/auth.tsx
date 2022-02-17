4894d18392ba8140a8ecff481ab138661ae023d3
Matheus Schneider <matheus.ssm@gmail.com>
2022-02-17 18:38:08 -0300

    add: authentication logic

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,11 @@
-import React, { createContext, useContext, ReactNode } from 'react';
+import React, { createContext, useContext, ReactNode, useState } from 'react';
+import auth from '@react-native-firebase/auth';
+import { Alert } from 'react-native';
 
-type AuthContextData = {};
+type AuthContextData = {
+  signIn: (email: string, password: string) => Promise<void>;
+  isSigningIn: boolean;
+};
 
 type AuthProviderProps = {
   children: ReactNode;
@@ -9,7 +14,37 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [isSigningIn, setIsSigningIn] = useState(false);
+
+  async function signIn(email: string, password: string) {
+    if (!email || !password) {
+      return Alert.alert('Login', 'Informe o e-amil e a senha');
+    }
+
+    setIsSigningIn(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((account) => {
+        console.log(account);
+      })
+      .catch((error) => {
+        const { code } = error;
+
+        if (code === 'auth/user-not-found' || code === 'auth/wrong-password') {
+          return Alert.alert('Login', 'E-mail e/ou senha inválidos.');
+        } else {
+          return Alert.alert('Login', 'Não foi possível realizar o login.');
+        }
+      })
+      .finally(() => setIsSigningIn(false));
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isSigningIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { KeyboardAvoidingView, Platform } from 'react-native';
 
 import brandImg from '@assets/brand.png';
@@ -13,8 +13,17 @@ import {
   ForgotPasswordLabel,
 } from './styles';
 import { Button } from '@components/Button';
+import { useAuth } from '@hooks/auth';
 
 export function SignIn() {
+  const { isSigningIn, signIn } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSignIn() {
+    signIn(email, password);
+  }
+
   return (
     <Container>
       <KeyboardAvoidingView
@@ -29,14 +38,25 @@ export function SignIn() {
             type='secondary'
             autoCorrect={false}
             autoCapitalize='none'
+            onChangeText={setEmail}
           />
 
-          <Input placeholder='Senha' type='secondary' secureTextEntry />
+          <Input
+            placeholder='Senha'
+            type='secondary'
+            secureTextEntry
+            onChangeText={setPassword}
+          />
 
           <ForgotPasswordButton>
             <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
           </ForgotPasswordButton>
-          <Button title='Entrar' type='secondary' />
+          <Button
+            title='Entrar'
+            type='secondary'
+            onPress={handleSignIn}
+            isLoading={isSigningIn}
+          />
         </Content>
       </KeyboardAvoidingView>
     </Container>
