83af78d522d4f3f00ae25816c6a1d5955e9e597f
Matheus Schneider <matheus.ssm@gmail.com>
2022-02-19 18:46:02 -0300

    add: function to handle pick images from library

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Platform, TouchableOpacity } from 'react-native';
 import { BackButton } from '@components/BackButton';
 import { Photo } from '@components/Photo';
+import * as ImagePicker from 'expo-image-picker';
 import {
   Container,
   Header,
@@ -12,6 +13,23 @@ import {
 } from './styles';
 
 export function Product() {
+  const [image, setImage] = useState('');
+
+  async function handlePickImage() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4],
+      });
+
+      if (!result.cancelled) {
+        setImage(result.uri);
+      }
+    }
+  }
+
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
@@ -25,9 +43,13 @@ export function Product() {
       </Header>
 
       <Upload>
-        <Photo uri='' />
+        <Photo uri={image} />
 
-        <PickImageButton title='Carregar' type='secondary' />
+        <PickImageButton
+          title='Carregar'
+          type='secondary'
+          onPress={handlePickImage}
+        />
       </Upload>
     </Container>
   );
